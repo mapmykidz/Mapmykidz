e266b0b1288c687484d5176bed93acb2a0ad0ef0
Garage Admin <garage@example.com>
2025-08-16 12:30:23 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Mapmykidz/sw.js', { scope: '/Mapmykidz/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
